fe0c00daa077c28e91adcb3c23e2423feaf45730
shimopino <shimoke4869@gmail.com>
2022-11-20 21:48:52 +0900

    green: 予約一覧を表示するための順序付きリストを追加

--- Chapter2/Finish/src/Appointment.js
+++ Chapter2/Finish/src/Appointment.js
@@ -2,4 +2,10 @@ import React from "react";
 
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
-export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
+export const AppointmentsDayView = () => {
+  return (
+    <div id="appointmentsDayView">
+      <ol />
+    </div>
+  );
+};
