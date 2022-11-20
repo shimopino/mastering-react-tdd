cf3ab9e25175ae4ff1ed2975d104de9c4115f2ce
shimopino <shimoke4869@gmail.com>
2022-11-20 20:42:41 +0900

    refactor: 重複しているコンテナ作成箇所を共通処理か

--- Chapter1/Finish/test/Appointment.test.js
+++ Chapter1/Finish/test/Appointment.test.js
@@ -7,16 +7,20 @@ import { Appointment } from "../src/Appointment";
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
 describe("Appointment", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    // 特定のDOMイベントが、要素がドキュメントツリーの一部である場合にのみ
+    // 登録されるため、Documentに生成したDOMを所属させる
+    document.body.replaceChildren(container);
+  });
+
   it("renders the customer first name", () => {
     // 期待する実行方法を用意する
     // ここでコンポーネントができていなくても問題なし
     const customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    // 特定のDOMイベントが、要素がドキュメントツリーの一部である場合にのみ
-    // 登録されるため、Documentに生成したDOMを所属させる
-    document.body.replaceChildren(container);
-
     // React18から render 関数は非同期になっているため、
     // DOMを修正する前にテストが終了してしまわないように
     // 非同期レンダリングが完了するまで一時停止するヘルパー関数を実行
@@ -30,8 +34,6 @@ describe("Appointment", () => {
   it("render another customer first name", () => {
     const customer = { firstName: "Jordan" };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
 
     act(() => {
       ReactDOM.createRoot(container).render(component);
