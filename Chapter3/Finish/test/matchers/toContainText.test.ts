5b39a9e6e7987aaa1c9c1b4d7f11f262818a6658
shimopino <shimoke4869@gmail.com>
2022-11-21 03:03:32 +0900

    red: テストが失敗した際に表示するメッセージの検証を追加

--- Chapter3/Finish/test/matchers/toContainText.test.ts
+++ Chapter3/Finish/test/matchers/toContainText.test.ts
@@ -1,5 +1,7 @@
 import { toContainText } from "./toContainText";
 
+const stripTerminalColor = (text: string) => text.replace(/\x1B\[\d+m/g, "");
+
 describe("toContainText matcher", () => {
   it("returns pass is true when text is found in the given DOM element", () => {
     const domElement = {
@@ -19,5 +21,15 @@ describe("toContainText matcher", () => {
     const result = toContainText(domElement, "text to find");
 
     expect(result.pass).toBe(false);
+  });
+
+  it("returns a message that contains the source line if no match", () => {
+    const domElement = { textContent: "" };
+
+    const result = toContainText(domElement, "text to find");
+
+    expect(stripTerminalColor(result.message())).toContain(
+      `expect(element).toContainText("text to find")`
+    );
   });
 });
