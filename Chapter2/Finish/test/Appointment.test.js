1654414782a4e3d175d426c13a6889da41b8f7ca
shimopino <shimoke4869@gmail.com>
2022-11-20 21:43:24 +0900

    red: 予約一覧を表示するためのコンポーネントに対して失敗するテストを追加

--- Chapter2/Finish/test/Appointment.test.js
+++ Chapter2/Finish/test/Appointment.test.js
@@ -41,3 +41,24 @@ describe("Appointment", () => {
     expect(document.body.textContent).toContain("Jordan");
   });
 });
+
+describe("AppointmentDaysView", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) => {
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+  };
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentDayView appointments={[]} />);
+
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+});
