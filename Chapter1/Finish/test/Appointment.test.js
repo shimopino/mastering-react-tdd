50c09e057d038d12803871ccf77f7938b60d0688
shimopino <shimoke4869@gmail.com>
2022-11-20 20:08:43 +0900

    red: 期待通りに失敗していることを確認した状態で、対象コンポーネントの使い方を提示

--- Chapter1/Finish/test/Appointment.test.js
+++ Chapter1/Finish/test/Appointment.test.js
@@ -1,5 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 describe("Appointment", () => {
   it("renders the customer first name", () => {
+    // 期待する実行方法を用意する
+    // ここでコンポーネントができていなくても問題なし
+    const customer = { firstName: "Ashley" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    // 特定のDOMイベントが、要素がドキュメントツリーの一部である場合にのみ
+    // 登録されるため、Documentに生成したDOMを所属させる
+    document.body.appendChild(container);
+    ReactDOM.createRoot(container).render(component);
+
     expect(document.body.textContent).toContain("Ashley");
   });
 });
