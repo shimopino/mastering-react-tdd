98d40422d2e7f67b1bb4b961f8631baef2f3404c
shimopino <shimoke4869@gmail.com>
2022-11-20 22:00:04 +0900

    red: Appointmentsの時刻が表示されていることを確認するための、失敗するテストを追加

--- Chapter2/Finish/test/Appointment.test.js
+++ Chapter2/Finish/test/Appointment.test.js
@@ -81,4 +81,18 @@ describe("AppointmentDaysView", () => {
     const listChildren = document.querySelectorAll("ol > li");
     expect(listChildren).toHaveLength(2);
   });
+
+  it("renders the time of each appointment", () => {
+    const today = new Date();
+    const twoAppointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) },
+    ];
+
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+
+    const listChildren = document.querySelectorAll("li");
+    expect(listChildren[0].textContent).toEqual("12:00");
+    expect(listChildren[1].textContent).toEqual("13:00");
+  });
 });
