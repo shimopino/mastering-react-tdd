2fc8613675694fcbe4993c907ba059c196e1f052
shimopino <shimoke4869@gmail.com>
2022-11-20 22:38:40 +0900

    green: ボタンをクリックした時に、配列のインデックスをPropsの配列に渡すように変更する

--- Chapter2/Finish/src/Appointment.js
+++ Chapter2/Finish/src/Appointment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const appointmentTimeOfDay = (startsAt) => {
   const [h, m] = new Date(startsAt).toTimeString().split(":");
@@ -8,12 +8,14 @@ const appointmentTimeOfDay = (startsAt) => {
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
 export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
   return (
     <div id="appointmentsDayView">
       <ol>
-        {appointments.map((appointment) => (
+        {appointments.map((appointment, index) => (
           <li key={appointment.startsAt}>
-            <button type="button">
+            <button type="button" onClick={() => setSelectedAppointment(index)}>
               {appointmentTimeOfDay(appointment.startsAt)}
             </button>
           </li>
@@ -22,7 +24,7 @@ export const AppointmentsDayView = ({ appointments }) => {
       {appointments.length === 0 ? (
         <p>There are no appointments scheduled for today.</p>
       ) : (
-        <Appointment {...appointments[0]} />
+        <Appointment {...appointments[selectedAppointment]} />
       )}
     </div>
   );
