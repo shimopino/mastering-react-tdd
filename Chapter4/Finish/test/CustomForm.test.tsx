ce45af906c43b5571b3216e64731ebc0a0dc2ab5
shimopino <shimoke4869@gmail.com>
2022-11-24 23:32:57 +0900

    red: input要素がDOM内に存在していることを確認するためのテストを追加

--- Chapter4/Finish/test/CustomForm.test.tsx
+++ Chapter4/Finish/test/CustomForm.test.tsx
@@ -12,4 +12,14 @@ describe("CustomForm", () => {
 
     expect(form()).not.toBeNull();
   });
+
+  it("renders the first name field as a text box", () => {
+    render(<CustomForm />);
+
+    // @ts-expect-error 最終的にラベルでの取得に変更する
+    const field = (form() as HTMLFormElement).elements.firstName;
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual("INPUT");
+    expect(field.type).toEqual("text");
+  });
 });
