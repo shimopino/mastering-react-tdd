de0725f08704c7c4dc815778afcd2df9b7ca5580
shimopino <shimoke4869@gmail.com>
2022-11-24 23:57:38 +0900

    green: CustomFormにPropsを追加

--- Chapter4/Finish/src/CustomForm.tsx
+++ Chapter4/Finish/src/CustomForm.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-export const CustomForm = () => (
+export const CustomForm = ({
+  original,
+}: {
+  original: { firstName: string };
+}) => (
   <form>
-    <input type="text" name="firstName" />
+    <input type="text" name="firstName" value={original.firstName} />
   </form>
 );
